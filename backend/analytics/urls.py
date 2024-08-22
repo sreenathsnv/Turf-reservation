2dc139c1b67bec480c78c08b678fa6829902104b
athulraja <athulraja1822@gmail.com>
2024-08-22 13:00:00 +0530

    Errors resolved

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -1,13 +1,13 @@
 # urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from analytics.views import PlayerAnalysisViewSet 
+# from analytics.views import PlayerAnalysisViewSet 
 from .views import (DailyBookingsView, WeeeklyBookingsView,
                     MonthlyBookingView,RevenueView,
                     ExportBookingsCSVView,performance_metrics_over_time,
                     overall_performance_rating,player_skill_radar_chart)
 router = DefaultRouter()
-router.register(r'player-analysis', PlayerAnalysisViewSet)
+# router.register(r'player-analysis', PlayerAnalysisViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -16,8 +16,8 @@ urlpatterns = [
     path('bookings/monthly/', MonthlyBookingView, name='monthly-bookings'),
     path('revenue/', RevenueView, name='revenue'),
     path('export/bookings/', ExportBookingsCSVView, name='export-bookings-csv'),
-    path('api/player/<int:player_id>/performance-metrics-over-time/', performance_metrics_over_time, name='performance_metrics_over_time'),
-    path('api/player/<int:player_id>/overall-performance-rating/', overall_performance_rating, name='overall_performance_rating'),
-    path('api/player/<int:player_id>/skill-radar-chart/', player_skill_radar_chart, name='player_skill_radar_chart'),
+    path('player/<int:player_id>/performance-metrics-over-time/', performance_metrics_over_time, name='performance_metrics_over_time'),
+    path('player/<int:player_id>/overall-performance-rating/', overall_performance_rating, name='overall_performance_rating'),
+    path('player/<int:player_id>/skill-radar-chart/', player_skill_radar_chart, name='player_skill_radar_chart'),
 ]
 
